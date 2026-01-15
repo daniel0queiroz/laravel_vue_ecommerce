c4987da8301514811ff8ef36e07e6808f2968a74
Daniel <daniel.olivqueiroz@outlook.com>
2026-01-15 09:05:35 -0300

    feat: 13 install vuex and create store

---
+++ src/laravel_vue_ecommerce/vue-admin-backend/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        test: "abcdefgh",
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+});
+
+export default store;
